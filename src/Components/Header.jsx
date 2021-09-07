de6f19c6207e9481a4cfe14789ba03ed20caaa37
Shivkumar Chauhan <pankajsinghat95@gmail.com>
2021-09-07 12:13:33 +0530

    Author added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,15 +5,17 @@ function Header(object) {
     return (
         <>
             <header>
-                <h1 onClick={()=>{
+                <h1 onClick={() => {
                     object.changepage(0)
                 }}>Note App</h1>
                 <nav>
-                    <div onClick={()=>{
-                    object.changepage(1)
+                    <div onClick={() => {
+                        object.changepage(1)
                     }}>Recents</div>
                 </nav>
             </header>
+            <div class="author">Created By : Shivkumar Chauhan</div>
+
         </>
     )
 }
